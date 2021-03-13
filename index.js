8d8a95e15bb13a82ad00537c3840f20c14f4435c
Juan Pablo <uncorreodeinternet@gmail.com>
2021-03-13 18:39:01 -0500

    [#2] Added Env and loading system

---
+++ index.js
@@ -0,0 +1,14 @@
+const { existsSync } = require('fs');
+const { resolve } = require('path');
+const yaml = require('yamljs');
+
+function load(key) {
+  if (process.env[key.name] === undefined) process.env[key.name] = key.value;
+};
+
+const fileName = resolve(resolve(process.cwd()), `env.yml`);
+if (!existsSync(fileName)) return;
+const yamlFile = yaml.load(fileName);
+yamlFile.forEach(key => load(key));
+
+console.log(process.env.APP_NAME)
