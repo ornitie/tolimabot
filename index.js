54c3f638d12e305b6eef7b12763d8cea929c1c60
Juan Pablo <uncorreodeinternet@gmail.com>
2021-03-20 20:29:31 -0500

    [#5] Added express server

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const { existsSync } = require('fs');
 const { resolve } = require('path');
 const yaml = require('yamljs');
+const express = require('express');
+
+const app = express();
 
 function load(key) {
   if (process.env[key.name] === undefined) process.env[key.name] = key.value;
-};
+}
 
-const fileName = resolve(resolve(process.cwd()), `env.yml`);
+function loadFile() {
+  const fileName = resolve(resolve(process.cwd()), 'env.yml');
 
-if (!existsSync(fileName)) return;
+  if (!existsSync(fileName)) return;
 
-const yamlFile = yaml.load(fileName);
-console.log(yamlFile)
-yamlFile.forEach(key => load(key));
+  const yamlFile = yaml.load(fileName);
+  console.log(yamlFile);
+  yamlFile.forEach((key) => load(key));
+}
 
-console.log(process.env.APP_NAME)
+loadFile();
+
+console.log(process.env.APP_NAME);
+
+app.listen(3000, () => {
+  console.log('listening on 3000');
+});
