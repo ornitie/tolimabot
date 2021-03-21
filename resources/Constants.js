88235950b5d4a699f3fa2ccd3695c748e02aa248
Juan Pablo <uncorreodeinternet@gmail.com>
2021-03-21 14:35:14 -0500

    [#12] Added behaviour to get next fixtures for the team

--- resources/Constants.js
+++ resources/Constants.js
@@ -2,4 +2,10 @@ module.exports = {
   BASE_URL: 'https://api-football-v1.p.rapidapi.com/v3',
   LEAGUE_ID: 239,
   TEAM_ID: 1142,
+  LEAGUE_FLAG: 'league=',
+  TEAM_FLAG: 'team=',
+  SEASON_FLAG: 'season=',
+  STATUS_FLAG: 'status=',
+  RAPID_HOST_HEADER: 'x-rapidapi-host',
+  RAPID_KEY_HEADER: 'x-rapidapi-key',
 };
--- resources/FootballAPIResource.js
+++ resources/FootballAPIResource.js
@@ -1,1 +1,38 @@
+const FootballAPIResource = module.exports;
+
 const BaseResource = require('./BaseResource');
+const {
+  BASE_URL,
+  LEAGUE_FLAG,
+  LEAGUE_ID,
+  TEAM_FLAG,
+  SEASON_FLAG,
+  STATUS_FLAG,
+  TEAM_ID,
+  RAPID_HOST_HEADER,
+  RAPID_KEY_HEADER,
+} = require('./Constants');
+
+const NOT_STARTED = 'NS';
+const FIXTURES = '/fixtures';
+const ResourceName = 'FootballAPIResource';
+const CURRENT_SEASON = '2021'; // TODO
+
+FootballAPIResource.getNextFeatures = async () => {
+  const { RAPID_HOST, RAPID_KEY } = process.env;
+
+  const options = {
+    caller: ResourceName,
+    url: `${BASE_URL + FIXTURES}?${LEAGUE_FLAG + LEAGUE_ID}`
+    + `&${SEASON_FLAG + CURRENT_SEASON}&${STATUS_FLAG + NOT_STARTED}&`
+    + `${TEAM_FLAG + TEAM_ID}`,
+    headers: {
+      [RAPID_HOST_HEADER]: RAPID_HOST,
+      [RAPID_KEY_HEADER]: RAPID_KEY,
+    },
+  };
+
+  const raw = await BaseResource.basicGet(options);
+
+  return raw.data;
+};
