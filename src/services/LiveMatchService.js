07af6515a60970b33eb5bad492b8cef340c28021
Juan Pablo <uncorreodeinternet@gmail.com>
2021-06-05 16:38:16 -0500

    [#56] Added Player and assistant on event

--- src/services/LiveMatchService.js
+++ src/services/LiveMatchService.js
@@ -4,6 +4,13 @@ const LiveMatchService = module.exports;
 
 const TwitterAPIResource = require('../resources/TwitterAPIResource');
 
+const buildMessage = ({
+  detail, time, player, team, assist,
+}, activeFixture) => (
+  `Minuto ${time.elapsed} en ${activeFixture.venue.city}: ${detail} de ${player.name} del equipo ${team}.${
+    assist.name ? ` Asistencia de ${assist.name}` : ''}`
+);
+
 LiveMatchService.PublishNewEvents = (events, activeFixture) => Promise
-  .map(events, ({ detail, time, player }) => TwitterAPIResource
-    .postTweet(`Minuto ${time.elapsed} Nuevo Evento en ${activeFixture.venue.city}: ${detail} de ${player.name}`));
+  .map(events, (event) => TwitterAPIResource
+    .postTweet(buildMessage(event, activeFixture)));
